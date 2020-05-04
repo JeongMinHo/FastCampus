4c647e7c4c5b5a2b41d4446f47b737e9eeccef63
JeongMinHo <wjdalsgh95@nate.com>
2020-05-05 01:45:32 +0900

    feat: parsing

--- MyNetflix/MyNetflix/Sources/Search/SearchViewController.swift
+++ MyNetflix/MyNetflix/Sources/Search/SearchViewController.swift
@@ -21,14 +21,12 @@ class SearchViewController: UIViewController {
 }
 
 // MARK: - UISearchBarDelegate
-
 extension SearchViewController: UISearchBarDelegate {
     
     private func dismissKeyboard() {
         searchBar.resignFirstResponder()
     }
-    
-    
+
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         dismissKeyboard()
         
@@ -36,8 +34,9 @@ extension SearchViewController: UISearchBarDelegate {
         
         // 검색 API
         SearchAPI.search(searchTerm) { movies in
-            // collectionView로 표현
             
+            // collectionView로 표현
+            print("--> 몇 개 : \(movies.count)")
         }
     }
 }
@@ -76,17 +75,50 @@ class SearchAPI {
                 completion([])
                 return
             }
+            
+            let movies = SearchAPI.parseMovies(resultData)
+            completion(movies)
+            print(movies.count)
         }
         dataTask.resume()
+    }
+    
+    static func parseMovies(_ data: Data) -> [Movie] {
+        let decoder = JSONDecoder()
+        
+        do {
+            let response = try decoder.decode(Response.self, from: data)
+            let movies = response.movies
+            return movies
+        } catch let error {
+            print("Parsing error : \(error.localizedDescription)")
+            return []
+        }
     }
 }
 
 // Movie 객체, Response 객체
-struct Movie {
+struct Movie: Codable {
+    let title: String
+    let director: String
+    let thumbnailPath: String
+    let previewURL: String
     
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case director = "artistName"
+        case thumbnailPath = "artworkUrl100"
+        case previewURL = "previewUrl"
+    }
 }
 
-struct Response {
+struct Response: Codable {
+    let resultCount: Int
+    let movies: [Movie]
     
+    enum CodingKeys: String, CodingKey {
+        case resultCount
+        case movies = "results"
+    }
 }
 
