0be184c6d53b3bdca89b3131c9a065276ab68633
JeongMinHo <wjdalsgh95@nate.com>
2020-05-05 00:07:56 +0900

    feat: iTunes URL

--- MyNetflix/MyNetflix/Sources/Search/SearchViewController.swift
+++ MyNetflix/MyNetflix/Sources/Search/SearchViewController.swift
@@ -30,9 +30,63 @@ extension SearchViewController: UISearchBarDelegate {
     
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        
         dismissKeyboard()
         
         guard let searchTerm = searchBar.text, searchTerm.isEmpty == false else { return }
+        
+        // 검색 API
+        SearchAPI.search(searchTerm) { movies in
+            // collectionView로 표현
+            
+        }
     }
 }
+
+// 검색 API
+class SearchAPI {
+    
+    static func search(_ term: String, completion: @escaping ([Movie]) -> Void) {
+        
+        let session = URLSession(configuration: .default)
+        
+        // URL
+        guard var urlComponents = URLComponents(string: "https://itunes.apple.com/search?") else { return }
+        let mediaQuery = URLQueryItem(name: "media", value: "movie")
+        let entityQuery = URLQueryItem(name: "entity", value: "movie")
+        let termQuery = URLQueryItem(name: "term", value: term)
+        
+        urlComponents.queryItems?.append(mediaQuery)
+        urlComponents.queryItems?.append(entityQuery)
+        urlComponents.queryItems?.append(termQuery)
+        
+        guard let requestURL = urlComponents.url else { return }
+        
+        let dataTask = session.dataTask(with: requestURL) { (data, response, error) in
+            
+            let successRange = 200..<300
+            
+            guard error == nil,
+                let statusCode = (response as? HTTPURLResponse)?.statusCode
+                , successRange.contains(statusCode) else {
+                    completion([])
+                    return
+            }
+            
+            guard let resultData = data else {
+                completion([])
+                return
+            }
+        }
+        dataTask.resume()
+    }
+}
+
+// Movie 객체, Response 객체
+struct Movie {
+    
+}
+
+struct Response {
+    
+}
+
