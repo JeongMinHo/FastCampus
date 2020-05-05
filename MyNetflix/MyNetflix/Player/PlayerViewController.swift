b8f6287abb5c53da3d9cb4e5364e3974770c9749
JeongMinHo <wjdalsgh95@nate.com>
2020-05-06 02:36:33 +0900

    feat: player 재생 구현

--- MyNetflix/MyNetflix/Player/PlayerViewController.swift
+++ MyNetflix/MyNetflix/Player/PlayerViewController.swift
@@ -18,18 +18,62 @@ class PlayerViewController: UIViewController {
     
     // MARK: - IBAction
     @IBAction func playButtonTouchUpInside(_ sender: UIButton) {
+    
+        if player.isPlaying {
+            pause()
+        } else {
+            play()
+        }
         playButton.isSelected.toggle()
     }
     
     @IBAction func closeButtonTouchUpInside(_ sender: UIButton) {
+        reset()
         dismiss(animated: true, completion: nil)
     }
     
     // MARK: - Value
     let player = AVPlayer()
     
+    // MARK: - Method
+    func play() {
+        player.play()
+        playButton.isSelected = true
+    }
+    
+    func pause() {
+        player.pause()
+        playButton.isSelected = false
+    }
+    
+    func reset() {
+        pause()
+        player.replaceCurrentItem(with: nil)
+    }
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .landscapeRight
     }
     
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        playerView.player = self.player
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        play()
+        playButton.isSelected = false
+    }
+    
+}
+
+extension AVPlayer {
+    var isPlaying: Bool {
+        guard self.currentItem != nil else { return false }
+        return self.rate != 0
+    }
 }
