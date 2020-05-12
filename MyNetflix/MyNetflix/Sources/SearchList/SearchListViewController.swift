85318925954edeea02ea2ff2fee75a1caf801392
JeongMinHo <wjdalsgh95@nate.com>
2020-05-13 00:32:54 +0900

    feat: JSON decoding

--- MyNetflix/MyNetflix/Sources/SearchList/SearchListViewController.swift
+++ MyNetflix/MyNetflix/Sources/SearchList/SearchListViewController.swift
@@ -16,13 +16,52 @@ class SearchListViewController: UIViewController {
     
     // MARK: - Value
     let db = Database.database().reference().child("searchHistory")
+    var searchTerms: [SearchTerm] = []
+    var cellIdentifier: String = "SearchList"
     
     // MARK: - View Life Cycle
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         db.observeSingleEvent(of: .value) { (snapshot) in
-            print("value : \(snapshot)")
+            guard let searchHistory = snapshot.value as? [String: Any] else { return }
+            
+            let data = try! JSONSerialization.data(withJSONObject: Array(searchHistory.values), options: [])
+            
+            let decoder = JSONDecoder()
+            let searchTerms = try! decoder.decode([SearchTerm].self, from: data)
+            self.searchTerms = searchTerms
+            self.tableView.reloadData()
         }
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(searchTerms)
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension SearchListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchTerms.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let dequeued = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        guard let cell = dequeued as? SearchListCell else { return dequeued}
+        
+        
+        cell.searchTerm.text = searchTerms[indexPath.row].term
+        
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension SearchListViewController: UITableViewDelegate {
+    
 }
