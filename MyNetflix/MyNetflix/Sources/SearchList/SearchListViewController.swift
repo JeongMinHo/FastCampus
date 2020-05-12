726fd966dddecd7ef3533495cb3faf1f6ad39b18
JeongMinHo <wjdalsgh95@nate.com>
2020-05-12 22:29:10 +0900

    feat: add Firebase

---
+++ MyNetflix/MyNetflix/Sources/SearchList/SearchListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SearchListViewController.swift
+//  MyNetflix
+//
+//  Created by jeongminho on 2020/05/12.
+//  Copyright © 2020 jeongminho. All rights reserved.
+//
+
+import UIKit
+
+class SearchListViewController: UIViewController {
+    
+    
+}
